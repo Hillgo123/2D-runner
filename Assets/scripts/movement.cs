b4fddb797a5f285c45ddd31d34ce95133b52b273
Leo Bergqvist <leo@bergqvist.se>
2023-02-01 16:42:27 +0100

    jump

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
 
     public float jump_height = 1;
 
+    private bool is_jumping = false;
+
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKey("space") && is_jumping == false)
         {
-            transform.position += Vector3.up * jump_height;
+            rb.AddForce(Vector2.up * jump_height, ForceMode2D.Impulse);
+            is_jumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            is_jumping = false;
         }
     }
 }
